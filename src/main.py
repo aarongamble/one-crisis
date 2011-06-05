1f6b53eaa1c676c32752dbc2cebdb5364b212b76
Aaron Gamble <jaarongamble@gmail.com>
2011-06-05 10:48:36 -0700

    person stuff

--- src/main.py
+++ src/main.py
@@ -10,6 +10,12 @@ from django.utils import simplejson
 
 from model import *
 
+'''
+limit one person to each user
+the entire code base uses this assumption
+'''
+USER_PERSON_LIMIT = 1
+
 class MainPage(webapp.RequestHandler):
     def get(self):
         template_values = {}
@@ -17,6 +23,16 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
         
 class People(webapp.RequestHandler):
+    
+    @staticmethod
+    def get_current_user_person():
+        user = users.get_current_user()
+        person = db.Query(Person).filter('user =', user).fetch(limit=USER_PERSON_LIMIT)
+        if person:
+            return person[0] # Only one person for each user
+        else:
+            return None
+        
     def get(self):
         user = users.get_current_user()
         person = Person.get(self.request.get('id'))
@@ -28,7 +44,7 @@ class People(webapp.RequestHandler):
             self.response.out.write(template.render(path, template_values))
             return
         
-        if user and user.key() == person.key():
+        if person.user == user:
             edit = True     # Viewing self, mark as edit
         else:
             edit = False
@@ -55,11 +71,11 @@ class Profile(webapp.RequestHandler):
         logging.debug("user")
         logging.debug(user)
         #Check if the user already has a person object
-        person = Person.get(user.user_id())
+        person = People.get_current_user_person()
         
         if not person:
             #Create a new Person instance for this user
-            person = Person(key_name=user.key())
+            person = Person()
             person.put()
         
         template_values = {'person': person}
